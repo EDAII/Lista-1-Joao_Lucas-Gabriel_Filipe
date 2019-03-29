a5c66e92cfed4d6cc06ed9a92ed705e53015dd13
Zarbielli <jlfz06@gmail.com>
2019-03-28 23:00:31 -0300

    adding homepage and time of search
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RadioButton } from "react-native-paper";
-import BarGraph from './components/chart';
+import BarGraph from "./components/chart";
 
-global.arr = []
+global.arr = [];
 export default class App extends React.Component {
-
   state = {
-    checked: "first",
+    checked: " ",
     array: [],
-    fill: 'rgb(134, 65, 244)',
-    data: [0 ,0, 0]
+    fill: "rgb(134, 65, 244)",
+    data: [0, 0, 0]
   };
-  
-  makeArray = (tamanho) => {
-    global.arr = [...Array(tamanho).keys()]
-  }
-  
+
+  makeArray = tamanho => {
+    global.arr = [...Array(tamanho).keys()];
+  };
+
   amandaUm = (tam, radio) => {
     let vector = [];
     this.makeArray(tam);
@@ -26,40 +25,36 @@ export default class App extends React.Component {
     let binario = this.binarySearch(ar);
     let inter = this.interpolationSearch(ar);
     this.setState({ array: vector });
-    console.log('ASS: '+sequencial)
+    console.log("ASS: " + sequencial);
     return [sequencial, binario, inter];
   };
-  
-  compilador = (tam, radio) => {
-    let estado = this.amandaUm(tam, radio)
-    this.chamaADois(estado)
-  }
 
-  chamaADois = (estado) => {
+  compilador = (tam, radio) => {
+    let estado = this.amandaUm(tam, radio);
+    this.chamaADois(estado);
+  };
+
+  chamaADois = estado => {
     this.amandaDois(estado);
-  }
-  amandaDois = (estado) => {
+  };
+  amandaDois = estado => {
     console.log("entrou amanda 2");
-    console.log(estado)
-    this.setState({fill: 'rgb(134, 65, 244)', data: estado});  
-  }
+    console.log(estado);
+    this.setState({ fill: "rgb(134, 65, 244)", data: estado });
+  };
 
   seqSearch = array => {
     let begin = global.nativePerformanceNow();
-    let item = array[array.length - 1]
+    let item = array[array.length - 1];
     for (let i = 0; i < array.length; i++) {
       if (array[i] == item) {
         let fim = global.nativePerformanceNow();
-        console.log("--------- Sequencial--------");
-        console.log("Posição " + i + " Tempo: " + (fim - begin));
-        console.log("---------------------------");
         let tempo = fim - begin;
         return tempo;
       }
     }
   };
 
-  
   binarySearch = array => {
     let begin = global.nativePerformanceNow();
     let start = 0;
@@ -77,9 +72,6 @@ export default class App extends React.Component {
     }
 
     let fim = global.nativePerformanceNow();
-    console.log(" --------- Binaria ---------");
-    console.log("Posição: " + middle + " Tempo: " + (fim - begin));
-    console.log("----------------------------");
     return array[middle] !== value ? -1 : fim - begin;
   };
 
@@ -103,15 +95,11 @@ export default class App extends React.Component {
     }
 
     let fim = global.nativePerformanceNow();
-    console.log(" --------- Interpolada ---------");
-    console.log("Posição: " + middle + " Tempo: " + (fim - begin));
-    console.log("----------------------------");
     return array[middle] !== value ? -1 : fim - begin;
   };
 
   render() {
     const { checked } = this.state;
-    console.log("dentro do render "+this.state.data)
     return (
       <View style={styles.container}>
         <View style={styles.titulo}>
@@ -146,14 +134,27 @@ export default class App extends React.Component {
           <Text style={{ fontSize: 16 }}> 10K </Text>
           <Text style={{ fontSize: 16 }}> 1M </Text>
         </View>
-        <BarGraph data={() =>this.state.data} svg={()=>this.state.fill}/>        
+        <BarGraph data={() => this.state.data} svg={() => this.state.fill} />
+        <View style={styles.nomeAlgo}>
+        {this.state.data.map((value, i) => {     
+           console.log("Entered");                     
+           return (<Text key={i} >{ value.toFixed(5) }</Text>) 
+        })}
+        </View>
+        <View style={styles.nomeAlgo}>
+          <Text>Sequencial </Text>
+          <Text> Binária </Text>
+          <Text> Interpolada </Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: "column"
+  },
   radioButton: {
     backgroundColor: "#fff",
     marginTop: 30,
@@ -165,6 +166,14 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   radioText: {
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  nomeAlgo: {
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  numeros: {
     flexDirection: "row",
     justifyContent: "space-evenly"
   }
